5d48087adc744e39f02deed58d6e000ab46c2757
Guido Barbaglia <guido.barbaglia@gmail.com>
2014-11-27 11:10:02 +0100

    Root variable added.

--- geobricks_ui_download.js
+++ geobricks_ui_download.js
@@ -1,7 +1,9 @@
+var root = '../modules/';
+
 define(['jquery',
         'mustache',
-        'text!../modules/geobricks_ui_download/html/templates.html',
-        'i18n!../modules/geobricks_ui_download/nls/translate',
+        'text!' + root + 'geobricks_ui_download/html/templates.html',
+        'i18n!' + root + 'geobricks_ui_download/nls/translate',
         'chosen',
         'bootstrap'], function ($, Mustache, templates, translate, chosen) {
 
